75d62d862c7a1813f8ba0caa5f5cdb7ecbb21903
hajar285 <hajar.mohammad285@gmail.com>
2022-07-06 17:20:40 +0300

    commit

--- index.php
+++ index.php
@@ -1,54 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+echo '<table border="1">';
+$start_row=1;
+$array = array();
+if(($csv_file = fopen("C:\\xampp\\htdocs\\test\\monthly_payrolls.csv","r"))!==False){
+    $total =0; 
+    while(($read_data=fgetcsv($csv_file,1000,","))!==False){
+        $total =0; 
+        $column_count=count($read_data);
+        echo "<tr>";
+        $start_row++;
+        for($c=0;$c<$column_count;$c++){
+            echo "<td>".$read_data[$c]."</td>";
+            $total+=intval($read_data[$c]);
+            
+        }
+        echo "<td >". $total."</td>";
+    }
+    fclose($csv_file);
+}
+echo '</table>';
 
-    <link rel="stylesheet" href="style.css">
 
-    <title>Document</title>
-</head>
-
-<body>
-    </div>
-    <div id="container">
-        <span id="title">Registration Form</span>
-        <form action="register.php" method="POST">
-            <div id="details">
-                <div id="input-details">
-                    <span id="content">Full Name </span>
-                    <input type="text" name="fullname" placeholder="Enter your full name">
-                </div>
-                <div id="input-details">
-                    <span id="content">Username </span>
-                    <input type="text" name="uname" placeholder="Enter your username ">
-                </div>
-                <div id="input-details">
-                    <span id="content">Email </span>
-                    <input type="email" name="email" placeholder="Enter your email">
-                </div>
-                <div id="input-details">
-                    <span id="content">Phone number </span>
-                    <input type="text" name="phone" placeholder="Enter your phone">
-                </div>
-                <div id="input-details">
-                    <span id="content">Password </span>
-                    <input type="password" name="pass" placeholder="Enter your password">
-                </div>
-                <div id="input-details">
-                    <span id="content">Confirm Password </span>
-                    <input type="password" name="cpass" placeholder="Confirm password ">
-                </div>
-
-            </div>
-            <div id="button">
-                <input type="submit" value="Register">
-            </div>
-        </form>
-    </div>
-
-</body>
-
-</html>
+?>
